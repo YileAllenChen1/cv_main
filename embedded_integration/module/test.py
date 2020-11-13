c74b4d0aa6642b85ca33716a5fa85ff5c7827c5d
KewlKris <chambca@gmail.com>
2020-11-13 03:10:57 -0600

    Remove old import

--- embedded_integration/module/test.py
+++ embedded_integration/module/test.py
@@ -2,8 +2,6 @@ import embedded_integration.util as util
 from embedded_integration.command import String_Echo, Debug
 from embedded_integration.integration_wrapper import IntegrationWrapper
 
-from random import shuffle
-
 iw = IntegrationWrapper()
 text = iw.echoString('Hello there!')
 print('Got: ' + text)
