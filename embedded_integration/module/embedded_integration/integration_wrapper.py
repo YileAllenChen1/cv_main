db512e1a4aab3de8a8d654136e32ee4cb78eba72
KewlKris <chambca@gmail.com>
2020-11-13 02:59:35 -0600

    Remove read and write from IntegrationWrapper
    Raw reads/writes should never be performed externally. Everything should be implemented as Command classes.

--- embedded_integration/module/embedded_integration/integration_wrapper.py
+++ embedded_integration/module/embedded_integration/integration_wrapper.py
@@ -12,16 +12,6 @@ class IntegrationWrapper():
         else:
             self.__serial = util.openFreshSerial(port)
     
-    def write(self, data: bytes):
-        '''Write raw bytes to the serial port.
-        '''
-        self.__serial.write(data)
-    
-    def read(self, length: int) -> bytes:
-        '''Read raw bytes from the serial port.
-        '''
-        return self.__serial.read(length)
-    
     def echoString(self, text: str) -> str:
         '''Bounce a string to and from the board.
 
